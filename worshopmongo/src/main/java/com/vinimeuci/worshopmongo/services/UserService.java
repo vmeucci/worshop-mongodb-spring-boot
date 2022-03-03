f1d3adddca1e80ae408c952aa7b5ec59ccb8ddba
vmeucci <viniciusmeuci@icloud.com>
2022-03-03 17:49:01 -0300

    Insert user created

--- worshopmongo/src/main/java/com/vinimeuci/worshopmongo/services/UserService.java
+++ worshopmongo/src/main/java/com/vinimeuci/worshopmongo/services/UserService.java
@@ -1,21 +1,36 @@
 package com.vinimeuci.worshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Service;
 
 import com.vinimeuci.worshopmongo.domain.User;
+import com.vinimeuci.worshopmongo.dto.UserDTO;
 import com.vinimeuci.worshopmongo.repository.UserRepository;
+import com.vinimeuci.worshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
