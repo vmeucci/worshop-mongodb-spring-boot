d3898c74257dc34fc7864ed7eab5a1c76caf5f1a
vmeucci <viniciusmeuci@icloud.com>
2022-03-08 15:33:51 -0300

    Query method to find a post by title

---
+++ worshopmongo/src/main/java/com/vinimeuci/worshopmongo/resources/util/URL.java
@@ -0,0 +1,17 @@
+package com.vinimeuci.worshopmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+
+	public static String decodeParam(String txt) {
+		try {
+			return URLDecoder.decode(txt, "UTF-8");
+			
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+		
+	}
+}
