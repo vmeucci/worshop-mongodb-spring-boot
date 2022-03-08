2f241923c33bd474e46eba61b47a11a82b450c6f
vmeucci <viniciusmeuci@icloud.com>
2022-03-08 12:36:56 -0300

    Posts findById implementation

---
+++ worshopmongo/src/main/java/com/vinimeuci/worshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.vinimeuci.worshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinimeuci.worshopmongo.domain.Post;
+import com.vinimeuci.worshopmongo.repository.PostRepository;
+import com.vinimeuci.worshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
