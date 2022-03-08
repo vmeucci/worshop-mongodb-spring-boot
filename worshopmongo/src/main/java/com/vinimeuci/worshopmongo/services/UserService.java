f6a3a3a0f47ab3f7784ab4a9979e4fb7f480949e
vmeucci <viniciusmeuci@icloud.com>
2022-03-08 00:25:28 -0300

    Creating the user update

--- worshopmongo/src/main/java/com/vinimeuci/worshopmongo/services/UserService.java
+++ worshopmongo/src/main/java/com/vinimeuci/worshopmongo/services/UserService.java
@@ -25,15 +25,26 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		userRepository.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
