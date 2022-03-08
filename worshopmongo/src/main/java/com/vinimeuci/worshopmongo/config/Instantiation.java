68955ee60d5cd6eec0bd9e009cf4ad7eb698aa6c
vmeucci <viniciusmeuci@icloud.com>
2022-03-08 03:28:58 -0300

    Creating Author as user

--- worshopmongo/src/main/java/com/vinimeuci/worshopmongo/config/Instantiation.java
+++ worshopmongo/src/main/java/com/vinimeuci/worshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.vinimeuci.worshopmongo.domain.Post;
 import com.vinimeuci.worshopmongo.domain.User;
+import com.vinimeuci.worshopmongo.dto.AuthorDTO;
 import com.vinimeuci.worshopmongo.repository.PostRepository;
 import com.vinimeuci.worshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("01/03/2022"), "Vamos jogar!", "Estou entrando no Elden Ring", maria);
-		Post post2 = new Post(null, sdf.parse("23/04/2020"), "Estou indo tomar a vacina", "Vacinado!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("01/03/2022"), "Vamos jogar!", "Estou entrando no Elden Ring", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/04/2020"), "Estou indo tomar a vacina", "Vacinado!",  new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
