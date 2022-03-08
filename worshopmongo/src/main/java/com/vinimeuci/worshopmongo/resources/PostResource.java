eaa75818271efd4f628930428b4a3cf7b18342a1
vmeucci <viniciusmeuci@icloud.com>
2022-03-08 19:16:37 -0300

    Creating the final query method and ending the project

--- worshopmongo/src/main/java/com/vinimeuci/worshopmongo/resources/PostResource.java
+++ worshopmongo/src/main/java/com/vinimeuci/worshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.vinimeuci.worshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +28,7 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 
 	}
-	
+
 	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
@@ -36,5 +37,16 @@ public class PostResource {
 
 	}
 
-	
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullsearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+
+	}
 }
