0cfe8ee3ba6610621cf9d4c72648894d46722b98
vmeucci <viniciusmeuci@icloud.com>
2022-03-08 12:27:51 -0300

    Referencing the users posts

--- worshopmongo/src/main/java/com/vinimeuci/worshopmongo/config/Instantiation.java
+++ worshopmongo/src/main/java/com/vinimeuci/worshopmongo/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/04/2020"), "Estou indo tomar a vacina", "Vacinado!",  new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
