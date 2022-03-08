54a99c4c975469caa4ff7e7e0df91de0df7c6a2e
vmeucci <viniciusmeuci@icloud.com>
2022-03-08 12:45:46 -0300

    Adding comments on posts

--- worshopmongo/src/main/java/com/vinimeuci/worshopmongo/config/Instantiation.java
+++ worshopmongo/src/main/java/com/vinimeuci/worshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.vinimeuci.worshopmongo.domain.Post;
 import com.vinimeuci.worshopmongo.domain.User;
 import com.vinimeuci.worshopmongo.dto.AuthorDTO;
+import com.vinimeuci.worshopmongo.dto.CommentDTO;
 import com.vinimeuci.worshopmongo.repository.PostRepository;
 import com.vinimeuci.worshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("01/03/2022"), "Vamos jogar!", "Estou entrando no Elden Ring", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/04/2020"), "Estou indo tomar a vacina", "Vacinado!",  new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Excelente jogo", sdf.parse("04/03/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Estou viciado", sdf.parse("02/03/2022"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Finalmente estamos vacinados", sdf.parse("10/05/2020"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
